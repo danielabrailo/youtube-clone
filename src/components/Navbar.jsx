e2b3efec0fc867091cd1241234627da872abb0a0
Daniela Brailovsky <dbrailo@gmail.com>
2023-12-21 15:48:38 +0100

    added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
 
-function Navbar() {
-  return <div>Navbar</div>;
-}
+import { logo } from "../utils/constants";
+import SearchBar from "./SearchBar";
+
+const Navbar = () => (
+  <Stack
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{
+      positions: "sticky",
+      background: "#000",
+      top: 0,
+      justifyContent: "space-between",
+    }}
+  >
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="logo" height={45} />
+    </Link>
+    <SearchBar></SearchBar>
+  </Stack>
+);
 
 export default Navbar;
